0736b3a479bd9eef69f5cf2c16f7d9a4e0d43a10
Agyeiwaa00 <scoogwinkworth@gmail.com>
2024-04-06 22:37:48 +0200

    Picture-Display-API

--- src/PicsDisplay.js
+++ src/PicsDisplay.js
@@ -1,25 +1,32 @@
-import { useEffect, useState } from "react";
+import {useCallback, useEffect, useState} from "react";
 
 
-const getPicDisplay = async() => {
-const response = await fetch('https://dog.ceo/api/breeds/image/random')
-const dog = await response.json()
-return dog.message
+const API_URL = `https://dog.ceo/api/breeds/image/random`
+
+function PicsDisplay() {
+  // API: https://dog.ceo/dog-api/
+  const [picDisplay, setPicDisplay] = useState('')
+  const getPicDisplay = async () => {
+  const data = await fetch(API_URL);
+  const {message, status} = await data.json()
+    if (status === "success") {
+      setPicDisplay(message)
+    }
+  }
+
+  useEffect(() => {getPicDisplay().catch(err => {
+        return err
+      })
+  }, [])
+
+  return (
+    <div className='pic-Display m-20'>
+      <img src={picDisplay} alt='Doggie' className="w-50 h-60"/>
+      <button className='border-2 px-2 border-gray-400 m-5' onClick={() => getPicDisplay()}> Click to display
+      </button>
+    </div>
+
+  )
 }
 
-function PicsDisplay(){
-    // API: https://dog.ceo/dog-api/
-    const[picDisplay, setPicDisplay]=useState('')
-    useEffect(() =>{
-    getPicDisplay().then(pic=>setPicDisplay(pic))
-    }, []
-    )
-    return (
-        <div className='pic-Display m-20'>
-            <img src={picDisplay} alt='Doggie' className="w-50 h-60"/>
-           <button className='border-2 px-2 border-gray-400 m-5' onclick={async e => setPicDisplay(await getPicDisplay())}> Click to display </button>
-        </div>
-        
-    )
-}
 export default PicsDisplay;
